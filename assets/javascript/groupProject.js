e75ac7261c995649860a8f9ebf63b0f3bdf18657
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-09-17 11:00:06 -0500

    uploading HTML

---
+++ assets/javascript/groupProject.js
@@ -0,0 +1,13 @@
+
+// this is ths firebase configuration that we all can use
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAeTLSZjy3Tm0U9UoRTePBVg2HD3mYUqQM",
+    authDomain: "firstgroupprojec-1473951653941.firebaseapp.com",
+    databaseURL: "https://firstgroupprojec-1473951653941.firebaseio.com",
+    storageBucket: "firstgroupprojec-1473951653941.appspot.com",
+    messagingSenderId: "1058292953682"
+  };
+  firebase.initializeApp(config);
+</script>
