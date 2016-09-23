c21a98553d0b9002cbcee8b04c0baf4f3bcac4a8
Rajyalakshmi C <chamarthi226@gmail.com>
2016-09-23 14:01:20 -0500

    firebase username problem corrected

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -267,8 +267,12 @@ $("#userName").change(function() {
         var location = snapshot.val();
         console.log(location);
         for (var key in location) {
-            cityArray.push(location[key].cityName);
-            console.log("cityName:" + location[key].cityName);
+        	 //removes duplicate citynames
+             if(cityArray.indexOf(location[key].cityName) === -1) {
+	                cityArray.push(location[key].cityName);
+	                console.log("cityName:" + location[key].cityName);
+            }
+
         }
         console.log("cityArray" + cityArray);
 
@@ -281,12 +285,11 @@ $("#city").autocomplete({
 });
 
 function storeInFirebase() {
-    userName = $("#userName").val();
     cityName = $("#city").val();
     category = $("#category").val();
     state = $("#state").val();
 
-    console.log("userName" + userName);
+    console.log("userName2" + userName);
     console.log("cityName" + cityName);
     console.log("category" + category);
     console.log("state" + state);
@@ -360,6 +363,11 @@ function storeInFirebase() {
 
 
 });
+//getting username
+$("#enterWebsite").on("click",function(){
+         userName = $("#userName").val();
+         console.log("userName1"+userName);
+         });
 
 
 
