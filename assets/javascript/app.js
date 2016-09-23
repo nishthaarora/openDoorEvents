a547e3f522e7deb20526b6e287a0914ef9ce39f1
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-09-23 09:14:29 -0500

    changing the layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,14 +5,12 @@ var zipCode;
 
 // parameter object to be passed to eventful API for making an api call
 var apiParameters = {
-
     app_key: "3sqmmtWF3swnsGxH",
     page_size: 2,
     sort_order: "popularity"
 
 };
-var p1, p2, p0,temp;
-
+var p1, p2, p0, temp;
 
 $(document).ready(function() {
 
@@ -24,27 +22,27 @@ $(document).ready(function() {
     function getEventsAndPinn(event, date) {
         event.preventDefault();
 
-    // capturing user inputs
+        // capturing user inputs
         state = $('#state').val();
         city = $('#city').val();
         category = $('#category').val();
         date = date || $('#date').val();
-        
-    // making an API call by sending user inputs
+
+        // making an API call by sending user inputs
         apiParameters.location = '';
-        if( city ) {
-            apiParameters.location = city + ','; 
+        if (city) {
+            apiParameters.location = city + ',';
         }
 
-        if ( state ) {
+        if (state) {
             apiParameters.location += state;
         }
         apiParameters.category = category;
         apiParameters.date = date;
-        
 
 
-        // These are the variables for the promise that we are making 
+
+        // These are the variables for the promise that we are making
         p0 = getWeather(zipCode);
         p1 = getEvents("/events/search", apiParameters);
         p2 = initialiseGoogleMap(document.getElementById('map'), {
@@ -55,19 +53,19 @@ $(document).ready(function() {
             }
         });
 
-        p0.then(getTempFromWeatherObj,function(err){
-              console.log(err);
-            });
+        p0.then(getTempFromWeatherObj, function(err) {
+            console.log(err);
+        });
         // sequence of promises
         p1.then(pushEventsToArray)
-          .then(geocodeEvents,function(err){
-              console.log(err);
+            .then(geocodeEvents, function(err) {
+                console.log(err);
             });
 
 
 
         /* This is our second promise that we are making in this we are initialising our googlemaps and sending the params
-        divID and map parameters which are defined in p2 promise above. divId is the id in HTML for maps and mapParamObject 
+        divID and map parameters which are defined in p2 promise above. divId is the id in HTML for maps and mapParamObject
         are defining the initial zoom and lat and lon for the map.
         */
         function initialiseGoogleMap(divID, mapParamObj) {
@@ -82,10 +80,9 @@ $(document).ready(function() {
                     resolve({
                         map: map,
                         geocoder: geocoder,
-                        
+
                     });
-                }         
-                else {
+                } else {
                     reject("error");
                 }
 
@@ -93,11 +90,11 @@ $(document).ready(function() {
         }
 
 
-        /* once getEvents is executed then we are passing its values with data variable and further looping 
+        /* once getEvents is executed then we are passing its values with data variable and further looping
         through the objects of array and pushing it to our array i.e eventArr
         */
         function pushEventsToArray(data) {
-            
+
             eventArr = [];
             $('#tableBody').children().remove();
 
@@ -116,22 +113,22 @@ $(document).ready(function() {
             });
 
             updateDomTable();
-             // p2 is the second promise which is initialising my google maps.
+            // p2 is the second promise which is initialising my google maps.
             return p2;
         }
 
 
-        /* This is the first promise p1 to get all the events from eventful api 
-        and we are passing 2 arguments url and parameters a call back function 
+        /* This is the first promise p1 to get all the events from eventful api
+        and we are passing 2 arguments url and parameters a call back function
         with event data. We are getting the event data from api and sendng it to
          function pushEventsToArray with "data"
         */
         function getEvents(url, paramObj) {
 
-
             return new Promise(function(resolve, reject) {
 
                 EVDB.API.call(url, paramObj, function(eventData) {
+                    console.log(eventData)
 
                     if (eventData) {
                         resolve(eventData);
@@ -147,7 +144,7 @@ $(document).ready(function() {
         // This is the funtion where we are defining out markers and passing the values to googlemaps
         function geocodeEvents(data) {
 
-            
+
 
             eventArr.forEach(function(ele) {
 
@@ -160,10 +157,10 @@ $(document).ready(function() {
 
                         data.map.setCenter(results[0].geometry.location);
 
-                         var contentString ='<div>'+'Event: '+ele.eventName+'<br>'+
-                                        'Address: '+ele.eventAddress+'<br>'+
-                                        'Date: '+moment(ele.eventDate).format('MM DD YYYY, hh:mm a')+'<br>'+
-                                        'temp: '+temp+' F'+'</div>';
+                        var contentString = '<div>' + 'Event: ' + ele.eventName + '<br>' +
+                            'Address: ' + ele.eventAddress + '<br>' +
+                            'Date: ' + moment(ele.eventDate).format('MM DD YYYY, hh:mm a') + '<br>' +
+                            'temp: ' + temp + ' F' + '</div>';
 
                         var infowindow = new google.maps.InfoWindow({
                             content: contentString
@@ -186,7 +183,7 @@ $(document).ready(function() {
             });
         }
 
-// This ia function to get the weather from the third API i.e weather
+        // This ia function to get the weather from the third API i.e weather
         function getWeather(zipCode) {
 
             // This is our API Key
@@ -203,47 +200,91 @@ $(document).ready(function() {
 
     };
 
-// This function is to get the weather from the weather object which we are receiving in the above function
+    // This function is to get the weather from the weather object which we are receiving in the above function
     function getTempFromWeatherObj(response) {
         temp = response.main.temp;
-        
+
     }
 
 
-// function to update the table of events on the webpage
-    function updateDomTable(){
-      eventArr.forEach(function(ele){
-        $('#tableBody').append('<tr>'+
-                      '<td>'+ele.eventName+'</td>'+
-                      '<td>'+ele.eventAddress+'</td>'+
-                      '<td>'+ele.eventDate+'</td>'+
-                      '<td></td>'+
-                    '</tr>');
-      });    
+    // function to update the table of events on the webpage
+    function updateDomTable() {
+        eventArr.forEach(function(ele) {
+            $('#tableBody').append('<tr>' +
+                '<td>' + ele.eventName + '</td>' +
+                '<td>' + ele.eventAddress + '</td>' +
+                '<td>' + ele.eventDate + '</td>' +
+                '<td></td>' +
+                '</tr>');
+        });
     }
 
 
-// this function is enebling the functionality of the tabs: this week, today etc
-    $(document).on( 'click', '.viewSwitch', function ( event ) {
+    // this function is enabling the functionality of the tabs: this week, today etc
+    $(document).on('click', '.viewSwitch', function(event) {
         event.preventDefault();
         eventArr = [];
 
-        var linkHash = $( this ).find( 'a' ).attr( 'href' );
+        var linkHash = $(this).find('a').attr('href');
 
         // remove hash from front;
-        $('#submitButton').trigger( 'click', linkHash.slice(1, linkHash.length) );
+        $('#submitButton').trigger('click', linkHash.slice(1, linkHash.length));
 
     });
 
+    // Creating ImageTags dinamically
 
-// This function is hiding carousel from the web page and displaying the map on click of submit
+    var random = Math.floor(Math.random() * 8);
 
-    $('#submitButton').on('click', function(){
-        $('.eventDisplayTabs').show();    
-        $('.carousel').hide();    
+    function getImages() {
+        var imageDiv = $('<div class="row ImageRow">');
+        $('.frontPage').append(imageDiv);
+
+        for (var i = 0; i < 8; i++) {
+            var image = $('<img>');
+            image.addClass('randomImages col m3 s1');
+            image.attr('src', 'assets/images/' + i + '.jpg' || '.jpeg');
+            $('.ImageRow').append(image);
+
+        }
+
+
+    }
+
+    /* enterWebsite,below is a submit button displayed on the front page. If the user click on submit
+    then he will go to the mainPage currently he is on frontPage
+    */
+
+    $('#enterWebsite').on('click', function() {
+
+        var name = $('#userName').val();
+        console.log(name);
+        if (!name) {
+            $('.mainPage').hide();
+            // $('.userNameInput,.enterWebsite').append('Please enter your Name');
+            $('.userNameInput,.enterWebsite').attr('placeholder', 'Please enter your Name');
+        } else {
+            $('.mainPage').show();
+            $('.frontPage').hide();
+
+            var nameArr = name.split(" ");
+            nameArr.forEach(function(ele) {
+                $('.userNameInput,.enterWebsite').html('Welcome ' + ele.charAt(0).toUpperCase() +
+                    ele.substring(1) + '!');
+            });
+        }
+    });
+
+
+    // This function is hiding carousel from the web page and displaying the map on click of submit
+
+    $('#submitButton').on('click', function() {
+        $('.eventDisplayTabs').show();
+        $('.carousel').hide();
     })
 
 
+    getImages();
 
 
 });
