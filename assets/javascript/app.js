10200c87c6956b6fa4b322ac6a60a1f4cc69de1a
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-09-30 14:16:49 -0500

    firebase config

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -27,11 +27,11 @@ var apiParameters = {
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyAjiFcMAppFW9AqRusRp7dOSQSF-vVbYlA",
-    authDomain: "odeusers.firebaseapp.com",
-    databaseURL: "https://odeusers.firebaseio.com",
-    storageBucket: "odeusers.appspot.com",
-    messagingSenderId: "788239866664"
+    apiKey: "AIzaSyAeTLSZjy3Tm0U9UoRTePBVg2HD3mYUqQM",
+    authDomain: "firstgroupprojec-1473951653941.firebaseapp.com",
+    databaseURL: "https://firstgroupprojec-1473951653941.firebaseio.com",
+    storageBucket: "firstgroupprojec-1473951653941.appspot.com",
+    messagingSenderId: "1058292953682"
 };
 
 categoryArray = ['music', 'family', 'comedy', 'concerts', 'books', 'business', 'art', 'crafts'];
